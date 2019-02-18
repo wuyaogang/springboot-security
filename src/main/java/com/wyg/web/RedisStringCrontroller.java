1695663f65961b1847f28f50401196078042204d
TRITONSFS\A14857 <wuyg@tritonsfs.com>
2019-02-18 16:51:03 +0800

    添加简单的redis集成
    添加简单的redis集成

---
+++ src/main/java/com/wyg/web/RedisStringCrontroller.java
@@ -0,0 +1,48 @@
+package com.wyg.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by A14857 on 2019/2/18.
+ */
+@RestController
+@RequestMapping("/redis")
+public class RedisStringCrontroller {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @GetMapping("/setget.html")
+    public String env(String key, String data){
+        redisTemplate.opsForValue().set(key,data);
+        String str = redisTemplate.opsForValue().get(key);
+        return str;
+    }
+
+    /**
+     * springboot通过内置的序列化机制将字符串序列化成byte
+     * @param key
+     * @param value
+     * @return
+     */
+    public String connetionSet(final String key, final String value){
+        redisTemplate.execute(new RedisCallback() {
+            public Object doInRedis(RedisConnection conn) throws DataAccessException{
+
+                try {
+                    conn.set(key.getBytes(), value.getBytes());
+                    //((StringRedisConnection)conn).set(key,value);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        });
+        return "success";
+    }
+}
